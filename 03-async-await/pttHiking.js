353169024edb382931c28538aeffd61785b8fd4c
kenchen <4915185-kenchen0625@users.noreply.gitlab.com>
2020-02-03 16:58:49 +0800

    Incomplete: datetime format

--- 03-async-await/pttHiking.js
+++ 03-async-await/pttHiking.js
@@ -1,15 +1,45 @@
 const rp = require("request-promise");
 const cheerio = require('cheerio');
+const moment = require('moment');
 
-getPostsByPage()
-  .then(result => {
-    console.log(result);
-  });
+main();
 
-async function getPostsByPage(url = 'https://www.ptt.cc/bbs/Hiking/index.html'){
+async function main(){
+  const newestPage = await getTheNewestPage();
+  const results = [];
+  // console.log(newestPage);
+  // const start = true;
+  // while(start){
+  // }
+  const result = await getPostsByPage(newestPage);
+  const start = true;
+  const posts = result.posts;
+  // posts.forEach(el => {
+  //   if(el.date)
+  // })
+  console.log(result);
+}
+
+async function getTheNewestPage(){
   const options = {
     method: "GET",
-    url: url,
+    url: `https://www.ptt.cc/bbs/Hiking/index.html`,
+    transform: function(body){
+      return cheerio.load(body);
+    }
+  }
+
+  const $ = await rp(options);
+
+  const previous = $('#action-bar-container > div > div.btn-group.btn-group-paging > a:nth-child(2)').attr('href');
+  const newestPage = Number(previous.match(/[\d]+/)[0]) + 1;
+  return newestPage;
+}
+
+async function getPostsByPage(page){
+  const options = {
+    method: "GET",
+    url: `https://www.ptt.cc/bbs/Hiking/index${page}.html`,
     transform: function(body){
       return cheerio.load(body);
     }
@@ -23,7 +53,7 @@ async function getPostsByPage(url = 'https://www.ptt.cc/bbs/Hiking/index.html'){
         title: $(this).children().eq(1).text().trim(),
         uri: 'https://www.ptt.cc' + $(this).children().eq(1).children().attr('href'),
         author: $(this).children().eq(2).children().eq(0).text(),
-        date: $(this).children().eq(2).children().eq(2).text()
+        date: $(this).children().eq(2).children().eq(2).text().trim()
       });
     }
   });
@@ -33,5 +63,6 @@ async function getPostsByPage(url = 'https://www.ptt.cc/bbs/Hiking/index.html'){
   return {
     posts: posts,
     previous_page: 'https://www.ptt.cc' + previous,
+    previous_page_num: Number(previous.match(/[\d]+/)[0])
   };
 }
--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.log("Hello world");
+const moment = require('moment');
+console.log(moment('01/12/2016', 'DD/MM/YYYY', true).format());
