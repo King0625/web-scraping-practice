ab3db6f7be5e3bf4e5ae910f997ccb88f8028d73
kenchen <independentchen@gmail.com>
2020-02-04 17:24:20 +0800

    Add csv handling for sevenEleven crawler

--- 02-request-promise/sevenEleven.js
+++ 02-request-promise/sevenEleven.js
@@ -1,22 +1,34 @@
+const fs = require('fs');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 getAllcities()
-  .then(cities => {
-    const promises = [];
-    cities.forEach(el => {
-      promises.push(getStoresByCity(el));
-    })
-
-    Promise.all(promises)
-      .then(results => {
-        console.log(results);
-      })
-  })
-  .catch(err => {
-    console.log(err);
+.then(cities => {
+  const promises = [];
+  cities.forEach(el => {
+    promises.push(getStoresByCity(el));
   })
 
+  Promise.all(promises)
+  .then(results => {
+    // fs.writeFileSync('sevenEleven.csv', '城市,電號,店名,地址\n')
+    const concatArr = Array.prototype.concat(...results);
+    fs.writeFileSync('sevenEleven.csv', convertToCSV(concatArr));
+    console.log(convertToCSV(concatArr));
+  })
+})
+.catch(err => {
+  console.log(err);
+})
+
+
+function convertToCSV(arr) {
+  const array = [Object.keys(arr[0])].concat(arr)
+
+  return array.map(it => {
+    return Object.values(it).toString()
+  }).join('\n')
+}
 
 function getAllcities(){
   const options = {
@@ -59,6 +71,7 @@ function getStoresByCity(city){
       const results = [];
       $('tbody').children().slice(1).each(function(i, el){
         results[i] = {
+          城市: city,
           店號: $(this).children().eq(0).text(),
           店名: $(this).children().eq(1).text(),
           地址: $(this).children().eq(2).text()
