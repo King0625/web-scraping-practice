271fe9770cfc390a9e58ec1334a1920f88ea6f73
kenchen <independentchen@gmail.com>
2020-09-12 03:58:40 +0800

    Fix: improve ithome scraper

--- 03-async-await/ithome.js
+++ 03-async-await/ithome.js
@@ -8,24 +8,27 @@ const pageOptions = (url, page) => {
   };
 }
 
-getTeamMembers()
-.then(members => {
+main();
+
+async function main() {
+  const teamMembers = await getTeamMembers();
+  console.time("Fetching...");
   const promises = [];
-  members.forEach(function(el){
-    promises.push(getPostsByMember(el));
-  });
+  for (teamMember of teamMembers) {
+    promises.push(getPostsByMember(teamMember));
+  }
+  await Promise.all(promises)
+    .then(result => {
+      console.log(result);
+    })
+  console.timeEnd("Fetching...");
+}
 
-  Promise.all(promises)
-  .then(result => {
-    console.log(result);
-  })
-})
-
-async function getTeamMembers(){
+async function getTeamMembers() {
   const options = {
     method: "GET",
     url: "https://ithelp.ithome.com.tw/2020ironman/signup/team/78",
-    transform: function(body){
+    transform: function (body) {
       return cheerio.load(body);
     }
   }
@@ -33,35 +36,43 @@ async function getTeamMembers(){
   const $ = await rp(options);
 
   const members = [];
-  $('.contestants-list__title').each(function(i, el){
-    const urlPieces = $(this).attr('href').split('/'); 
-    urlPieces.splice(3, 0, "m/api");
-    const url = urlPieces.join('/')
+  $('.contestants-list__title').each(function (i, el) {
+    const url = $(this).attr('href')
     members.push(url);
   })
   return members;
 }
 
-async function getPostsByMember(url){
-  const options = pageOptions(url, 'undefined')
+async function getPostsByMember(url) {
+  const urlPieces = url.split('/');
+  urlPieces.splice(3, 0, "m/api");
+  const apiUrl = urlPieces.join('/');
 
+  const options = pageOptions(apiUrl, 'undefined')
   const body = await rp(options);
   const total_pages = body.paginator.total_pages
-  const articles = [];
-  for(let i = 1; i <= total_pages; i++){
-    const options = pageOptions(url, i);
-    const jsonResult = await rp(options);
-    articles.push(jsonResult.data.articles)
+  let lastPageData;
+  if (total_pages == 1) {
+    lastPageData = body;
+  } else {
+    const options = pageOptions(apiUrl, total_pages);
+    lastPageData = await rp(options);
   }
-  // const options1 = pageOptions(url, total_pages);
-  // const jsonResult = await rp(options1);
-  // const lastPageArticles = jsonResult.data.articles;
+  const lastPageArticles = lastPageData.data.articles;
+  const lastArticle = lastPageArticles[lastPageArticles.length - 1];
+  const lastArticleCreatedTimestamps = lastArticle.created_at;
+  const finishedToday = new Date().setHours(0, 0, 0, 0) == new Date(lastArticleCreatedTimestamps).setHours(0, 0, 0, 0);
   return {
-    account: body.data.user.account,
-    nickname: body.data.user.nickname,
-    avatar: body.data.user.avatar,
-    articles: Array.prototype.concat(...articles)
-    // articles: lastPageArticles[lastPageArticles.length - 1]
+    link: url,
+    account: lastPageData.data.user.account,
+    nickname: lastPageData.data.user.nickname,
+    avatar: lastPageData.data.user.avatar,
+    subject: lastPageData.data.ironman.subject,
+    topic_count: lastPageData.data.ironman.topic_count,
+    latest_article_subject: lastArticle.subject,
+    latest_article_link: `https://ithelp.ithome.com.tw/articles/${lastArticle.article_id}`,
+    latest_finished_datetime: new Date(lastArticleCreatedTimestamps).toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }),
+    has_finished_today: finishedToday
   }
 }
 
